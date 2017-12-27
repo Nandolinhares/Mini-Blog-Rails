7aec99771f2fd30ff3011365f11034ba3edbe8a7
Fernando Viana <fernandolinhares1920@globo.com>
2017-12-27 16:08:30 -0200

    Criado scaffold Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227180259) do
+ActiveRecord::Schema.define(version: 20171227180534) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.text     "body"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
 
 end
