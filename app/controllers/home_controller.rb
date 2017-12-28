cfd33989317116d8df49a325bd22b7734652c72d
Fernando Viana <fernandolinhares1920@globo.com>
2017-12-28 21:26:07 -0200

    Inserindo busca por categoria na home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 	def index
 		@posts = Post.most_recent
+		@categories = Category.all
+	end
+	def busca
+		@categoria_a_buscar = params[:category_id]
+    	@categorias_buscadas = Post.where "category_id like ?","%#{@categoria_a_buscar}%"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   mount Ckeditor::Engine => '/ckeditor'
-  get"posts/busca" => "posts#busca", as: :busca_post	
+  get "posts/busca" => "posts#busca", as: :busca_post
+  get "home/busca" => "home#busca", as: :busca_categories	
   resources :posts
   resources :categories
   root "home#index"
